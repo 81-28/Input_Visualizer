f310f15e3ece83847e16d615ae8cff64ee34a61e
Onion4070 <naonaosub.0704@gmail.com>
2025-09-11 00:16:10 +0900

    HIDレポートデータを16進数表示

--- Visualizer/main.cpp
+++ Visualizer/main.cpp
@@ -24,13 +24,53 @@ void open_serial_port(asio::serial_port& port, std::string port_name) {
 	}
 }
 
+
 int main() {
 	std::string port_name = "COM9";
 	const uint8_t start_byte = 0xAA;
-	const uint8_t stop_bytes = 0xBB;
+	const uint8_t end_byte   = 0xBB;
 
 	asio::io_context io;
 	asio::serial_port port(io);
+
+	open_serial_port(port, port_name);
+
+	while (true) {
+		try {
+			// 1. 開始バイトを待つ
+			uint8_t current_byte;
+			asio::read(port, asio::buffer(&current_byte, 1));
+			if (current_byte != start_byte) continue;
+
+			// 2. データ長を読む
+			uint8_t length;
+			asio::read(port, asio::buffer(&length, 1));
+
+			// 3. HIDデータを読む
+			std::vector<uint8_t> in_report(length, 0);
+			asio::read(port, asio::buffer(in_report));
+
+			// 4. 終了バイトか検証
+			asio::read(port, asio::buffer(&current_byte, 1));
+
+			// 終了バイト出ない場合は再度開始バイトを探す
+			if (current_byte != end_byte) {
+				std::cerr << "Warning: Invalid end byte. Packet discarded." << std::endl;
+				continue;
+			}
+
+			std::cout << "Received " << (int)length << " bytes";
+			for (const auto& byte : in_report) {
+				std::cout << std::hex << std::setw(2) << std::setfill('0') << (int)byte << " ";
+			}
+			std::cout << std::dec << std::endl;
+
+		}
+		catch (const std::exception& e) {
+			std::cerr << "Error: " << e.what() << std::endl;
+			std::this_thread::sleep_for(std::chrono::milliseconds(500));
+		}
+	}
 
 	open_serial_port(port, port_name);
 	return 0;
