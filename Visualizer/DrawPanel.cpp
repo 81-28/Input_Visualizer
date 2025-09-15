47de9561adb7ebaad2aa212e7871b1be7373e52b
Onion4070 <naonaosub.0704@gmail.com>
2025-09-16 02:25:43 +0900

    十字キーを追加

--- Visualizer/DrawPanel.cpp
+++ Visualizer/DrawPanel.cpp
@@ -23,6 +23,48 @@ void DrawPanel::ClearBackground(wxGCDC& gdc) {
     gdc.DrawRectangle(GetClientRect());
 }
 
+std::vector<wxPoint> CreateHomeBase(int cx, int cy, int size, double angle) {
+    std::vector<wxPoint> points(5);
+
+    double base[5][2];
+	if (angle == 0 || angle == M_PI) {
+        base[0][0] = 0, base[0][1] = 0; // 先端
+		base[1][0] = 1, base[1][1] = 1; // 右上
+		base[2][0] = 1, base[2][1] = 3; // 右下
+		base[3][0] = -1, base[3][1] = 3; // 左下
+		base[4][0] = -1, base[4][1] = 1; // 左上
+
+        if (angle == M_PI) {
+			for (int i = 0; i < 5; ++i) {
+				base[i][0] = -base[i][0];
+				base[i][1] = -base[i][1];
+			}
+        }
+    }
+
+    if (angle == M_PI / 2 || angle == 3 * M_PI / 2) {
+        base[0][0] = 0, base[0][1] = 0; // 先端
+        base[1][0] = -1, base[1][1] = 1; // 右上
+        base[2][0] = -3, base[2][1] = 1; // 右下
+        base[3][0] = -3, base[3][1] = -1; // 左下
+        base[4][0] = -1, base[4][1] = -1; // 左上
+
+        if (angle == 3 * M_PI / 2) {
+            for (int i = 0; i < 5; ++i) {
+                base[i][0] = -base[i][0];
+                base[i][1] = -base[i][1];
+            }
+        }
+    }
+
+    for (int i = 0; i < 5; ++i) {
+		int ix = base[i][0] * size;
+		int iy = base[i][1] * size;
+        points[i] = wxPoint(cx + ix, cy + iy);
+    }
+    return points;
+}
+
 void DrawPanel::OnPaint(wxPaintEvent& event) {
     wxAutoBufferedPaintDC dc(this);
     wxGCDC gdc(dc);
@@ -108,6 +150,51 @@ void DrawPanel::OnPaint(wxPaintEvent& event) {
 
 
     // 十字キー
+	gdc.SetPen(whitePenS);
+	gdc.SetBrush(*wxTRANSPARENT_BRUSH);
+	center_x = 110;
+	center_y = 170;
+	offset = 6;
+    short size = 8;
+    {
+		// 下
+        auto pts = CreateHomeBase(center_x, center_y + offset, size, 0);
+        gdc.DrawPolygon(pts.size(), pts.data());
+		if (gamepad.DPAD_DOWN) {
+			gdc.SetBrush(*wxWHITE);
+			gdc.DrawPolygon(pts.size(), pts.data());
+		}
+		gdc.SetBrush(*wxTRANSPARENT_BRUSH);
+    }
+	{
+		// 左
+		auto pts = CreateHomeBase(center_x - offset, center_y, size, M_PI / 2);
+		gdc.DrawPolygon(pts.size(), pts.data());
+		if (gamepad.DPAD_LEFT) {
+			gdc.SetBrush(*wxWHITE);
+			gdc.DrawPolygon(pts.size(), pts.data());
+		}
+		gdc.SetBrush(*wxTRANSPARENT_BRUSH);
+	}
+	{	// 上
+		auto pts = CreateHomeBase(center_x, center_y - offset, size, M_PI);
+		gdc.DrawPolygon(pts.size(), pts.data());
+		if (gamepad.DPAD_UP) {
+			gdc.SetBrush(*wxWHITE);
+			gdc.DrawPolygon(pts.size(), pts.data());
+		}
+		gdc.SetBrush(*wxTRANSPARENT_BRUSH);
+	}
+	{	// 右
+		auto pts = CreateHomeBase(center_x + offset, center_y, size, 3 * M_PI / 2);
+		gdc.DrawPolygon(pts.size(), pts.data());
+		if (gamepad.DPAD_RIGHT) {
+			gdc.SetBrush(*wxWHITE);
+			gdc.DrawPolygon(pts.size(), pts.data());
+		}
+		gdc.SetBrush(*wxTRANSPARENT_BRUSH);
+	}
+
 
     // -, +
 	gdc.SetPen(whitePenS);
@@ -128,6 +215,9 @@ void DrawPanel::OnPaint(wxPaintEvent& event) {
 		gdc.SetBrush(*wxWHITE);
 		gdc.DrawCircle(center_x - offset, center_y, radius);
 	}
+
+	// Capture, Home
+    // 見た目が悪いので実装しない
 
 
 	// A, B, X, Yボタン
