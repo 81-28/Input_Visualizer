bd36fe2a447f8aeff41080ef0d1b9b01ec0590e9
81-28 <eskrmc@gmail.com>
2025-09-27 23:48:38 +0900

    serial_default

--- RP2040.ino
+++ RP2040.ino
@@ -5,19 +5,15 @@
 // 1. Serial1 (RX1) で RP2350 からのデータを受信する。
 // 2. 受信したデータを、そのまま Serial2 (TX2) へ送信する (Leonardo行き)。
 // 3. 受信したデータを、そのまま Serial (USB) へ送信する (PCでのモニタリング用)。
+#define LED_PIN 25
 
 void setup() {
+  pinMode(LED_PIN, OUTPUT);
   // PCとのUSBシリアル通信 (モニタリング用)
   Serial.begin(115200);
-
-  // RP2350とのシリアル通信 (GP4=TX1, GP5=RX1)
-  Serial1.setTX(4);
-  Serial1.setRX(5);
+  // RP2350とのシリアル通信 (GP0=TX1, GP1=RX1)
   Serial1.begin(115200);
-
   // Leonardoとのシリアル通信 (GP8=TX2, GP9=RX2)
-  Serial2.setTX(8);
-  Serial2.setRX(9);
   Serial2.begin(115200);
 }
 
@@ -26,10 +22,12 @@ void loop() {
   if (Serial1.available()) {
     // 1バイト読み込む
     int data = Serial1.read();
+    Serial.print(data, HEX);
+    Serial.print("\n");
     
     // 読み込んだデータを、LeonardoとPCの両方へ送信する
     Serial2.write(data); // Leonardoへ
-    Serial.write(data);  // PCへ
+    // Serial.write(data);  // PCへ
   }
 }
 
